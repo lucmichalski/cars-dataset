61ae901296758c2d121e01323ac8c98fa40cc069
lucmichalski <luc.michalski@protonmail.com>
2020-04-08 09:00:04 +0000

    add draft web service for darknet, fixtures

--- .docker/darknet/download_data.sh
+++ .docker/darknet/download_data.sh
@@ -1,3 +1,8 @@
+#/bin/sh
+
+set -x
+set -e
+
 wget --output-document=sample.jpg https://cdn-images-1.medium.com/max/800/1*EYFejGUjvjPcc4PZTwoufw.jpeg
 wget --output-document=./models/coco.names https://raw.githubusercontent.com/AlexeyAB/darknet/master/data/coco.names
 wget --output-document=./models/yolov3.cfg https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov3.cfg
